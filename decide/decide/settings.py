d13f38da2b780fc47660c66b2b6a37e8352e3eac
Manuel Outeiriño <manueloute@gmail.com>
2021-11-29 18:36:51 +0100

    node install included into vagrant playbook and default settings updated

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'decide.urls'
